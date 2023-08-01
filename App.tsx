9ddabd5b1a566286e958c262338d6f277d38b8b3
Allana Larissa <larissaleonel98@gmail.com>
2023-08-01 13:41:57 -0300

    add

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
-import {requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject} from 'expo-location';
-import { useEffect, useState } from 'react';
+import {requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchPositionAsync, watchHeadingAsync, LocationAccuracy} from 'expo-location';
+import { SetStateAction, useEffect, useState, useRef } from 'react';
 import MapView, {Marker} from 'react-native-maps';
 
 
 export default function App() {
   const [location, setLocation] = useState< LocationObject| null>(null);
+  const mapref = useRef<MapView> (null)
   
   async function requestLocationPermissions(): Promise<JSX.Element> {
    const { granted } = await requestForegroundPermissionsAsync();
@@ -19,6 +20,16 @@ export default function App() {
     requestLocationPermissions();
 
    }, [])
+   
+   useEffect(()=>{
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response)=>{
+      setLocation(response)// MUDAR LOCALIZAÇÃO
+    })
+   },[])
   return (
     <View style={styles.container}>
       {
